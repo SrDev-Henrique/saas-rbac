29dfc6ab7a4df67b1c1e1df661b267bd8730d6cd
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-09-15 01:16:55 -0300

    feat: add create organization route

---
+++ apps/api/src/http/routes/orgs/create-organization.ts
@@ -0,0 +1,77 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { createSlug } from '@/utils/create-slug'
+
+export async function createOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Create a new organization',
+          security: [{ bearerAuth: [] }],
+          response: {
+            201: z.object({
+              message: z.string(),
+              organizationId: z.uuid(),
+            }),
+          },
+          body: z.object({
+            name: z.string(),
+            domain: z.string().nullish(),
+            shouldAttachUsersByDomain: z.boolean().default(false),
+            avatarUrl: z.string().nullish(),
+          }),
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const { name, domain, shouldAttachUsersByDomain, avatarUrl } =
+          request.body
+
+        if (domain) {
+          const organizationByDomain = await prisma.organization.findUnique({
+            where: {
+              domain,
+            },
+          })
+
+          if (organizationByDomain) {
+            throw new BadRequestError(
+              'Organization with this domain already exists',
+            )
+          }
+        }
+
+        const organization = await prisma.organization.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            domain,
+            shouldAttachUsersByDomain,
+            avatarUrl,
+            ownerId: userId,
+            members: {
+              create: {
+                userId,
+                role: 'ADMIN',
+              },
+            },
+          },
+        })
+
+        return reply.status(201).send({
+          message: 'Organization created successfully',
+          organizationId: organization.id,
+        })
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { env } from '@saas/env'
+import { createOrganization } from "./routes/orgs/create-organization"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -63,6 +64,7 @@ app.register(authenticateWithPassword)
 app.register(authenticateWithGithub)
 app.register(resetPassword)
 app.register(requestPasswordRecover)
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`HTTP server running on port ${env.SERVER_PORT}`)
