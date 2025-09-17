389722175e8f6139bcebfe253cb17c152f62435c
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-09-17 18:28:20 -0300

    feat: add get user pending invites route

---
+++ apps/api/src/http/routes/invites/get-pending-invites.ts
@@ -0,0 +1,82 @@
+import { auth } from '@/http/middlewares/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getPendingInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/pending-invites',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Get all user pending invites',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.uuid(),
+                  email: z.string(),
+                  role: z.string(),
+                  createdAt: z.date(),
+                  author: z
+                    .object({
+                      id: z.uuid(),
+                      name: z.string().nullable(),
+                      avatarUrl: z.url().nullable(),
+                    })
+                    .nullable(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        const invites = await prisma.invite.findMany({
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            email: user.email,
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        if (!invites) {
+          throw new BadRequestError('Invite not found.')
+        }
+
+        return { invites }
+      },
+    )
+}
