c2b5655f9f736b51fdfb960a7bb4f52dc27f6baf
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-09-17 17:32:48 -0300

    feat: add get all invites from an organization route

---
+++ apps/api/src/http/routes/invites/get-invites.ts
@@ -0,0 +1,88 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/invites',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Get all invites for an organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.uuid(),
+                  email: z.string(),
+                  role: z.string(),
+                  createdAt: z.date(),
+                  author: z
+                    .object({
+                      id: z.uuid(),
+                      name: z.string().nullable(),
+                      avatarUrl: z.url().nullable(),
+                    })
+                    .nullable(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Invite')) {
+          throw new UnauthorizedError(
+            'You are not allowed to get this organization invites.',
+          )
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            organizationId: organization.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        if (invites.length === 0) {
+          throw new BadRequestError('This organization has no invites.')
+        }
+
+        return reply.send({ invites })
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -35,6 +35,7 @@ import { updateMember } from './routes/members/update-member'
 import { removeMember } from './routes/members/remove-member'
 import { createInvite } from "./routes/invites/create-invite"
 import { getInviteDetails } from "./routes/invites/get-invite-details"
+import { getInvites } from "./routes/invites/get-invites"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -102,6 +103,7 @@ app.register(removeMember)
 
 app.register(createInvite)
 app.register(getInviteDetails)
+app.register(getInvites)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`HTTP server running on port ${env.SERVER_PORT}`)
