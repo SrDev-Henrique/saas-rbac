38c12d01b7acc9a19d0a88f9c17d913213f6c2ee
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-09-20 02:34:06 -0300

    feat: setup client http (ky)

--- apps/web/src/app/(auth)/sign-in/actions.tsx
+++ apps/web/src/app/(auth)/sign-in/actions.tsx
@@ -1,5 +1,14 @@
-"use server"
+'use server'
 
-export async function SignInWithEmail(data: FormData) {
-  console.log(Object.fromEntries(data))
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+export async function SignInWithPassword(data: FormData) {
+  const { email, password } = Object.fromEntries(data)
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  console.log(result)
 }
