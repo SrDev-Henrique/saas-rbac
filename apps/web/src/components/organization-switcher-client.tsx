03466b2bc63f2fe07742b0f5da41f58357532852
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-09-26 01:19:45 -0300

    feat: add organization navigation

---
+++ apps/web/src/components/organization-switcher-client.tsx
@@ -0,0 +1,85 @@
+// app/components/OrganizationSwitcher.client.tsx
+'use client'
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { PlusIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+interface Organizations {
+  role: 'ADMIN' | 'MEMBER' | 'BILLING'
+  id: string
+  name: string
+  slug: string
+  avatarUrl: string | null
+}
+
+export default function OrganizationSwitcherClient({
+  organizations,
+}: {
+  organizations: Organizations[]
+}) {
+  const router = useRouter()
+
+  return (
+    <div className="*:not-first:mt-2">
+      <Select
+        defaultValue=""
+        onValueChange={(value) => {
+          if (value === 'create-organization') {
+            router.push('/org/create')
+          } else if (value !== 'no-organization') {
+            router.push(`/org/${value}`)
+          }
+        }}
+      >
+        <SelectTrigger className="ps-2 [&>span]:flex [&>span]:items-center [&>span]:gap-2 [&>span_img]:shrink-0">
+          <SelectValue placeholder="Selecione uma organização" />
+        </SelectTrigger>
+
+        <SelectContent className="[&_*[role=option]]:ps-2 [&_*[role=option]]:pe-8 [&_*[role=option]>span]:start-auto [&_*[role=option]>span]:end-2 [&_*[role=option]>span]:flex [&_*[role=option]>span]:items-center [&_*[role=option]>span]:gap-2">
+          {organizations.length > 0 ? (
+            <SelectGroup>
+              <SelectLabel className="ps-2">
+                Mudar para outra organização
+              </SelectLabel>
+              {organizations.map((org) => (
+                <SelectItem key={org.id} value={org.slug}>
+                  <Avatar>
+                    <AvatarImage src={org.avatarUrl ?? ''} alt={org.name} />
+                    <AvatarFallback>
+                      {org.name.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <span className="line-clamp-1 truncate">{org.name}</span>
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          ) : (
+            <SelectGroup>
+              <SelectItem value="no-organization">
+                Nenhuma organização encontrada
+              </SelectItem>
+            </SelectGroup>
+          )}
+
+          <SelectSeparator />
+          <SelectGroup>
+            <SelectItem value="create-organization">
+              <PlusIcon /> Criar nova organização
+            </SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
--- apps/web/src/components/origin-ui/organization-switcher.tsx
+++ apps/web/src/components/origin-ui/organization-switcher.tsx
@@ -1,69 +1,12 @@
-import { useId, useMemo } from 'react'
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectSeparator,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-import { PlusIcon } from 'lucide-react'
 import { getOrganizations } from '@/http/get-organizations'
+import OrganizationSwitcherClient from '../organization-switcher-client'
 
 export default async function OrganizationSwitcher() {
   const { organizations } = await getOrganizations()
 
-  const defaultValue =
-    organizations.length > 0 ? organizations[0].id : 'no-organization'
   return (
-    <div className="*:not-first:mt-2">
-      <Select defaultValue={defaultValue}>
-        <SelectTrigger className="ps-2 [&>span]:flex [&>span]:items-center [&>span]:gap-2 [&>span_img]:shrink-0">
-          <SelectValue placeholder="Selecione uma organização" />
-        </SelectTrigger>
-        <SelectContent className="[&_*[role=option]]:ps-2 [&_*[role=option]]:pe-8 [&_*[role=option]>span]:start-auto [&_*[role=option]>span]:end-2 [&_*[role=option]>span]:flex [&_*[role=option]>span]:items-center [&_*[role=option]>span]:gap-2">
-          {organizations.length > 0 ? (
-            <SelectGroup>
-              <SelectLabel className="ps-2">
-                Mudar para outra organização
-              </SelectLabel>
-              {organizations.map((organization) => (
-                <SelectItem key={organization.id} value={organization.id}>
-                  <Avatar>
-                    <AvatarImage
-                      src={organization.avatarUrl ?? ''}
-                      alt={organization.name}
-                    />
-                    <AvatarFallback>
-                      {organization.name.charAt(0).toUpperCase() +
-                        organization.name.charAt(1).toUpperCase()}
-                    </AvatarFallback>
-                  </Avatar>
-                  <span className="line-clamp-1 truncate">
-                    {organization.name}
-                  </span>
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          ) : (
-            <SelectGroup>
-              <SelectItem value="no-organization">
-                Nenhuma organização encontrada
-              </SelectItem>
-            </SelectGroup>
-          )}
-          <SelectSeparator />
-          <SelectGroup>
-            <SelectItem value="create-organization">
-              <PlusIcon /> Criar nova organização
-            </SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+    <div>
+      <OrganizationSwitcherClient organizations={organizations} />
     </div>
   )
 }
