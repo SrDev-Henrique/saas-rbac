76789593f2dd8d454d8e8bdb2ab963cacda66a9b
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-09-29 03:09:07 -0300

    feat: validate domain value with regex

--- apps/web/src/app/(app)/org/create-org/schema.ts
+++ apps/web/src/app/(app)/org/create-org/schema.ts
@@ -4,7 +4,22 @@ export const createOrganizationSchema = z.object({
   name: z.string().min(1, {
     message: 'Nome é obrigatório',
   }),
-  domain: z.string().optional(),
+  domain: z
+    .string()
+    .optional()
+    .refine(
+      (value) => {
+        if (value) {
+          const domainRegex =
+            /^(?=.{1,253}$)(?!-)(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)+[A-Za-z]{2,63}$/
+          return domainRegex.test(value)
+        }
+        return true
+      },
+      {
+        message: 'Domínio inválido',
+      },
+    ),
   shouldAttachUsersByDomain: z.boolean().optional(),
   avatarUrl: z.string().nullable(),
 })
